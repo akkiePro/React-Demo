4110acc7d0313333d592076ed8633958bfb5baf8
Akash Prajapati <akashprajapati1849@gmail.com>
2024-05-18 18:09:41 +0530

    About Component with dark/light mode

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import NavBar from './components/NavBar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       {/* <NavBar/> */}
-      <NavBar title="MyApp" home="Home" about="About Us" />
-      <TextForm heading="Fill up form" />
+      <NavBar title="Text Converter" home="Home" about="About Us" />
+      {/* <TextForm heading="Fill up form" /> */}
+      <About />
     </>
   );
 }
